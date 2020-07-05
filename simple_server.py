9696a611c8d82ff0c1ea79517898e7abe2080051
Shubhasish <panda.subhasish007@gmail.com>
2020-07-05 15:57:39 +0530

    Hello world first commit

---
+++ simple_server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """Print 'Hello, world!' as the response body."""
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host="0.0.0.0")
