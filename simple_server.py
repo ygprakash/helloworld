7a8d32e1fcbeaf3703509adf512a9639c2650363
ygprakash <ygprakash27@gmail.com>
2021-04-07 14:08:53 +0200

    Update simple_server.py

--- simple_server.py
+++ simple_server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     """Print 'Hello, world!' as the response body."""
-    return 'hello world, Bravilor Bonamat!!'
+    return 'hello world, Bravilor Bonamat!! first dayyyyyyyyyyy'
 
 @app.route('/tri')
 def ri_tech():
