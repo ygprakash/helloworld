4d38044974fb116c3fff57c9e9c1807d2d74abe9
ygprakash <ygprakash27@gmail.com>
2021-04-12 09:43:13 +0200

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,11 @@
+from simple_server import app
+
+def test_hello():
+    response = app.test_client().get('/')
+    url_pattern_response = app.test_client().get('/trim')
+
+    assert response.status_code == 200
+    assert str(response.data) == 'hello world, Bravilor Bonamat!! '
+    #
+    assert url_pattern_response.status_code == 200
+    assert str(url_pattern_response.data) == '2nd page of Bravilor Bonamat!!!!!'
